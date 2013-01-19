d58d8b44ee35276f98c7539415e14b8b85a5265c
James York <james.s.york@gmail.com>
2013-01-20 00:34:19 +0200

    added more dynamic bits and bobs - to the layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,6 +13,11 @@ describe PagesController do
       get 'home'
       response.should have_selector("title", :content => "My Little Sample App | Home")   	
     end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
